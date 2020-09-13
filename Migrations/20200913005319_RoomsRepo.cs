10ce0db22fa71c7c76ac664d5ec6bea2d660e4b2
Jennifer B Jones <66266330+jenjones319@users.noreply.github.com>
2020-09-12 19:55:22 -0500

    fixed dependency inject rooms

---
+++ Migrations/20200913005319_RoomsRepo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASyncInn.Migrations
+{
+    public partial class RoomsRepo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
